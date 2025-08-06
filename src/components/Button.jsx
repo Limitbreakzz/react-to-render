d0f2035a231baa55d2133c30f6738b1f168d87d6
Limitbreak <lomphongsrathongmee@gmail.com>
2025-08-06 13:21:54 +0700

    8

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = () => {
   return (
     <>
       <Link 
-        to="/collection" 
+        to="/Products" 
         className="px-6 py-3 bg-blue-800 text-white rounded-lg shadow-md hover:bg-blue-600 transition duration-300"
       >
         รายการสินค้า
